297b1fe4a8ee6999c2f1ab71aa11480343369800
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 12:39:47 -0400

    Add favorite_numbers dictionary and print statement

---
+++ 6-10.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+        'Alex Bellos': [7, 9],
+        'Daniel Tammet': [8, 2],
+        'Michael Jordan': [6, 7],
+        'Babe Ruth': [23, 69, 68],
+        'Wayne Gretzky': [67],
+}
+
+for name, numbers in favorite_numbers.items():
+    print(f"{name}'s favorite numbers are: {numbers}")
