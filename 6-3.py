139860e5809a6d939f0ebf81f919947ce5680188
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 11:14:08 -0400

    Create glossary for data types with print statements
    Initialize a glossary dictionary with data types and their definitions, and print each definition.

---
+++ 6-3.py
@@ -0,0 +1,13 @@
+glossary = {
+        'integer': 'Whole Number',
+        'float': 'Decimal',
+        'String': 'Text',
+        'Boolean': 'True/False',
+        'List': 'Container that stores values'
+}
+
+print(glossary['integer'])
+print(glossary['float'])
+print(glossary['String'])
+print(glossary['Boolean'])
+print(glossary['List'])
