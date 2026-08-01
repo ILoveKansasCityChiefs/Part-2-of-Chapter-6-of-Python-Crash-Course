d366cc74640e8a20c2230e0ea1d80e6c7c84f3a7
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 12:21:31 -0400

    Add a list of people with their details

---
+++ 6-7.py
@@ -0,0 +1,8 @@
+person = {'first_name': 'Christopher', 'last_name': 'White', 'age': 14,'city': 'New York City'}
+person_2 = {'first_name': 'Ingrid', 'last_name': 'White', 'age': 45, 'city': 'Plymouth'}
+person_3 = {'first_name': 'John', 'last_name': 'Smith', 'age': 34, 'city': 'Chicago'}
+
+people = [person, person_2, person_3]
+
+for person in people:
+    print(person)
