bd824cc49868f79f6bd1e4d3d875810bef390dc8
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 11:09:04 -0400

    Add favorite_numbers dictionary and print statements
    Initialize favorite_numbers dictionary and print values.

---
+++ 6-2.py
@@ -0,0 +1,12 @@
+favorite_numbers = {
+        'Alex Bellos': 7, 
+        'Daniel Tammet': 'pi', 
+        'Michael Jordan': 23, 
+        'Babe Ruth': 3, 
+        'Wayne Gretzky': 99
+}
+print(favorite_numbers['Alex Bellos'])
+print(favorite_numbers['Daniel Tammet'])
+print(favorite_numbers['Michael Jordan'])
+print(favorite_numbers['Babe Ruth'])
+print(favorite_numbers['Wayne Gretzky'])
