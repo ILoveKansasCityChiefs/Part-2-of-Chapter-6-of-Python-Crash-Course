89c19b48ff3251322dd0f396d55ed042f5d431b3
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 12:35:13 -0400

    Add favorite_places dictionary and print favorites

---
+++ 6-9.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    "Alice": "Beach",
+    "Ben": "Mountains",
+    "Chloe": "Library"
+}
+
+for name, places in favorite_places.items():
+    print(f"{name.title()}'s favorite place to go to is: {places}")
