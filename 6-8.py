3cbc42d0290f0e2919dbe5fa6fb4b7bf45394a7a
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 12:32:20 -0400

    Add pet data and print each pet's details

---
+++ 6-8.py
@@ -0,0 +1,8 @@
+pet_1 = {'pet': 'Dog', 'owner': 'Barack Obama'}
+pet_2 = {'pet': 'Cat', 'owner': 'Bill Clinton'}
+pet_3 = {'pet': 'Pig', 'owner': 'Zsa Zsa Gabor'}
+pet_4 = {'pet': 'Chimpanzee', 'owner': 'Michael Jackson'}
+
+pets = [pet_1, pet_2, pet_3, pet_4]
+for pet in pets:
+    print(pet)
