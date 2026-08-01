8182fda9ff8c8461a2f15f82dd34eaee2fbb18a4
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 11:37:37 -0400

    Create 6-4.py

---
+++ 6-4.py
@@ -0,0 +1,10 @@
+glossary = {
+        'integer': 'Whole Number',
+        'float': 'Decimal',
+        'String': 'Text',
+        'Boolean': 'True/False',
+        'List': 'Container that stores values'
+}
+
+for word, meaning in glossary.items():
+    print(f"{word.title()} means {meaning} in Python")
