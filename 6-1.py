9988e9fa7c66a0a9bd0c035f1b94f5bdc824b522
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-08-01 10:56:12 -0400

    Add person dictionary and print details

---
+++ 6-1.py
@@ -0,0 +1,6 @@
+person = {'first_name': 'Christopher', 'last_name': 'White', 'age': 14,'city':'New York City'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
